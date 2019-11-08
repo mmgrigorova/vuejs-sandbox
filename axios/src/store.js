c289478d48614ca4aac82bbe2d5834d788cbc25a
mgrigorova <mmgrigorova@gmail.com>
2019-11-08 18:56:32 +0200

    Logout functionality

--- axios/src/store.js
+++ axios/src/store.js
@@ -1,38 +1,103 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from './axios-auth'
+import globalAxios from 'axios'
+import router from './router'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    idToken: null,
-    userId: null
-  },
-  mutations: {
-
-  },
-  actions: {
-    signup({commit}, authData){
-      axios.post('accounts:signUp?key=AIzaSyDfKOhaM-0dlgkU-hLcIdfeVLARthrx8L8', {
-        email: authData.email,
-        password: authData.password,
-        returnSecureToken: true
-      })
-          .then(res => console.log(res))
-          .catch(error => console.log(error));
+    state: {
+        idToken: null,
+        userId: null,
+        user: null
     },
-    login({commit}, authData){
-      axios.post('accounts:signInWithPassword?key=AIzaSyDfKOhaM-0dlgkU-hLcIdfeVLARthrx8L8', {
-        email: authData.email,
-        password: authData.password,
-        returnSecureToken: true
-      })
-          .then(res => console.log(res))
-          .catch(error => console.log(error));
-    }
-  },
-  getters: {
+    mutations: {
+        authUser(state, userData) {
+            state.idToken = userData.token;
+            state.userId = userData.userId
+        },
+        storeUser(state, user){
+            state.user = user;
+        },
+        clearAuthData(state){
+            state.idToken = null;
+            state.userId = null;
+        }
+    },
+    actions: {
+        signup({commit, dispatch}, authData) {
+            axios.post('accounts:signUp?key=AIzaSyDfKOhaM-0dlgkU-hLcIdfeVLARthrx8L8', {
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            }).then(res => {
+                console.log(res);
+                commit('authUser', {
+                    token: res.data.idToken,
+                    userId: res.data.localId
+                });
+                dispatch('storeUser', {authData})
+            }).catch(error => console.log(error));
+        },
+        login({commit}, authData) {
+            axios.post('accounts:signInWithPassword?key=AIzaSyDfKOhaM-0dlgkU-hLcIdfeVLARthrx8L8', {
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            }).then(res => {
+                console.log(res);
+                commit('authUser', {
+                    token: res.data.idToken,
+                    userId: res.data.localId
+                })
+            }).catch(error => console.log(error));
+        },
+        logout({commit}){
+            commit('clearAuthData');
+            router.replace('/signin');
+        },
+        storeUser({commit, state}, userData) {
+            if(!state.idToken){
+                return
+            }
+            
+            globalAxios.post('/users.json' + '?auth=' + state.idToken, userData)
+                .then(res => {
+                    console.log(res);
+                })
+                .catch(err => {
+                    console.log(err)
+                });
+        },
+        fetchUser({commit, state}) {
+            if(!state.idToken){
+                return
+            }
 
-  }
+            globalAxios.get('/users.json' + '?auth=' + state.idToken)
+                .then(res => {
+                    console.log(res);
+                    const data = res.data;
+                    const users = [];
+                    for (let key in data) {
+                        const user = data[key];
+                        user.id = key;
+                        users.push(user);
+                    }
+                    console.log(users);
+                    commit('storeUser', users[0]);
+                })
+                .catch(error => console.log(error));
+        }
+    }
+    ,
+    getters: {
+        user(state){
+            return state.user;
+        }, 
+        isAuthenticated(state){
+            return state.idToken !== null;
+        }
+    }
 })
