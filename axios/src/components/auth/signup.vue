106624425ec9cdffde9afe9f664fae8d0cf70534
mgrigorova <mmgrigorova@gmail.com>
2019-11-08 19:49:43 +0200

    Add validators

--- axios/src/components/auth/signup.vue
+++ axios/src/components/auth/signup.vue
@@ -2,12 +2,15 @@
     <div id="signup">
         <div class="signup-form">
             <form @submit.prevent="onSubmit">
-                <div class="input">
+                <div class="input" :class="{invalid: $v.email.$error}">
                     <label for="email">Mail</label>
                     <input
                             type="email"
                             id="email"
+                            @blur="$v.email.$touch()"
                             v-model="email">
+                    <!--<div>{{$v}}</div>-->
+                    <p v-if="!$v.email.email">Please provide valid email address</p>
                 </div>
                 <div class="input">
                     <label for="age">Your Age</label>
@@ -69,6 +72,8 @@
 </template>
 
 <script>
+    import {required, email} from 'vuelidate/lib/validators'
+
     export default {
         data() {
             return {
@@ -79,6 +84,12 @@
                 country: 'usa',
                 hobbyInputs: [],
                 terms: false
+            }
+        },
+        validations: {
+            email: {
+                required: required,
+                email: email
             }
         },
         methods: {
@@ -153,6 +164,15 @@
     .input select {
         border: 1px solid #ccc;
         font: inherit;
+    }
+    
+    .input.invalid input{
+        border: 1px solid red;
+        background-color: lightcoral;
+    }
+
+    .input.invalid label{
+        color: red;
     }
 
     .hobbies button {
--- axios/src/main.js
+++ axios/src/main.js
@@ -1,29 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import Vuelidate from 'vuelidate'
 
 import router from './router'
 import store from './store'
+
+Vue.use(Vuelidate)
 
 axios.defaults.baseURL = 'https://vuejs-axios-21cdb.firebaseio.com';
 // axios.defaults.headers.common['Authorization'] = 'generic token';
 axios.defaults.headers.get['Accepts'] = 'application/json';
 
 const reqInterceptor = axios.interceptors.request.use(config => {
-  console.log('Request interceptor ', config);
-  return config;
+    console.log('Request interceptor ', config);
+    return config;
 });
-const resInterceptor = axios.interceptors.response.use( res => {
-  console.log('Response interceptor ', res)
-  return res
+const resInterceptor = axios.interceptors.response.use(res => {
+    console.log('Response interceptor ', res)
+    return res
 });
 
 axios.interceptors.request.eject(reqInterceptor);
 axios.interceptors.request.eject(resInterceptor);
 
 new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
 })
